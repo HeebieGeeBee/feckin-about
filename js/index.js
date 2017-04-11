3075aa860a604dfffc2a5af897144e5e7bb07829
HeebieGeeBee <geoffbull29@hotmail.co.uk>
2017-04-11 20:17:21 +0200

    third

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(500, 500);
+	background(150,150,150);
+}
+
+function draw() {
+	fill("blue");
+	rect(100, 100, 50, 50);
+	fill("green");
+	textSize(50);
+	text("I'm bored", 150, 150);
+}
